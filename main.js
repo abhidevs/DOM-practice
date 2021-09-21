7423697ad5f0494df656f385034759b157c2343a
Abhik Das <abhikdas98765@gmail.com>
2021-09-21 21:40:09 +0530

    added search functionality

--- main.js
+++ main.js
@@ -1,20 +1,31 @@
 let form = document.getElementById("addForm");
 let itemList = document.getElementById("items");
+let filter = document.getElementById("filter");
 
-// form submit event handler
+// form submit event
 form.addEventListener("submit", addItem);
 
-// delete event handler
+// delete event
 itemList.addEventListener("click", removeItem);
 
-// Add item function
+// filter event
+filter.addEventListener("keyup", filterItems);
+
+// Add item functionality
 function addItem(e) {
   e.preventDefault();
   let newItem = document.getElementById("item");
+  let newDesc = document.getElementById("itemDesc");
 
   let item = document.createElement("li");
   item.className = "list-group-item";
   item.appendChild(document.createTextNode(newItem.value));
+
+  let lineBreak = document.createElement("br");
+  item.appendChild(lineBreak);
+  let itemDesc = document.createElement("small");
+  itemDesc.appendChild(document.createTextNode(newDesc.value));
+  item.appendChild(itemDesc);
 
   // Create and append the delete button for new item
   let deleteBtn = document.createElement("button");
@@ -32,7 +43,7 @@ function addItem(e) {
   console.log(item);
 }
 
-// Remove item function
+// Remove item functionality
 function removeItem(e) {
   if (e.target.classList.contains("delete")) {
     if (confirm("Are you Sure?")) {
@@ -40,4 +51,20 @@ function removeItem(e) {
       itemList.removeChild(itemToRemove);
     }
   }
+}
+
+// Filter item functionality
+function filterItems(e) {
+  let searchQuery = e.target.value.toLowerCase();
+  let items = itemList.getElementsByTagName("li");
+  Array.from(items).forEach(function (item) {
+    let itemName = item.firstChild.textContent;
+    let itemDesc = item.querySelector('small').textContent;
+
+    if (itemName.toLowerCase().indexOf(searchQuery) !== -1 || itemDesc.toLowerCase().indexOf(searchQuery) !== -1) {
+      item.style.display = "block";
+    } else {
+      item.style.display = "none";
+    }
+  });
 }
